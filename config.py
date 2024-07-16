3eaaff1e03e9f251f92f99707afacbae97eec149
GODxBGMI-HACKS <trpnaruto01@gmail.com>
2024-07-16 17:51:55 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,19 @@
+# Don't use quotes( " and ' )
+#SCRIPT BY GODxBGMI
+  
+#Enter Your Bot Token here get it from @botfarher
+BOT_TOKEN=("7353106103:AAEmWPOELbGBOlzJiKX-LUkS-WcHcqTYphc")
+
+  #Enter Your telegram username here without @
+OWNER_USERNAME=("GODxBGMI_OWNER")
+
+  #Enter your admin id here Get it from @missRose_bot by typing /info
+ADMIN_IDS=("1132426169")
+
+
+
+
+
+
+
+  
