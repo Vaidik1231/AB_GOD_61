9e5baeae31658ecb6b5ad46d477973a54f007263
Vaidik1231 <vaidiks694@gmail.com>
2024-07-19 22:36:34 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,10 +2,10 @@
 #SCRIPT BY GODxBGMI
   
 #Enter Your Bot Token here get it from @botfarher
-BOT_TOKEN=("7130547264:AAGikFw2LS9R6m9CXbAJSrUgUU4VTik_WR0")
+BOT_TOKEN=("7285790456:AAGuagS5COQMpf5G5WtJnv2kuMkFI-jpck4")
 
   #Enter Your telegram username here without @
-OWNER_USERNAME=("@ZOMBIExCHEAT")
+OWNER_USERNAME=("ToXicTaRuN00")
 
   #Enter your admin id here Get it from @missRose_bot by typing /info
 ADMIN_IDS=("1132426169")
